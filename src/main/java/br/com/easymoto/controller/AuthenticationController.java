3156cbdcb53bde844931407a08543803e3652711
Lu1zEdu <le6269199@gmail.com>
2025-09-22 19:29:04 -0300

    Security e JWT  funcionando junto com o inicio do thymeleaf com alguns erros de rota

---
+++ src/main/java/br/com/easymoto/controller/AuthenticationController.java
@@ -0,0 +1,33 @@
+package br.com.easymoto.controller;
+
+import br.com.easymoto.dto.AuthenticationDTO;
+import br.com.easymoto.dto.LoginResponseDTO;
+import br.com.easymoto.model.Funcionario;
+import br.com.easymoto.security.TokenService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthenticationController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private TokenService tokenService;
+
+    @PostMapping("/login")
+    public ResponseEntity login(@RequestBody AuthenticationDTO data){
+        var usernamePassword = new UsernamePasswordAuthenticationToken(data.login(), data.password());
+        var auth = this.authenticationManager.authenticate(usernamePassword);
+        var token = tokenService.generateToken((Funcionario) auth.getPrincipal());
+        return ResponseEntity.ok(new LoginResponseDTO(token));
+    }
+}
---
+++ src/main/java/br/com/easymoto/dto/LoginResponseDTO.java
@@ -0,0 +1,4 @@
+package br.com.easymoto.dto;
+
+public record LoginResponseDTO(String token) {
+}
