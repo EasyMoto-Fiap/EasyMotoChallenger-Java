11ac1bc17aa5e3ba27c8712eb4d9fbc768629ea0
Lu1zEdu <le6269199@gmail.com>
2025-09-23 16:20:48 -0300

    Swagger finalmente voltou a funcionar

--- src/main/resources/db/migration/V5__add-security-to-funcionario.sql
+++ src/main/resources/db/migration/V5__add-security-to-funcionario.sql
@@ -1,11 +1,8 @@
 ALTER TABLE funcionario ADD COLUMN password VARCHAR(255);
 ALTER TABLE funcionario ADD COLUMN cargo VARCHAR(20);
 
--- Adiciona uma filial para poder inserir funcionários
 INSERT INTO filial(nome_filial, cidade, estado, pais, endereco, empresa_id) VALUES ('Matriz SP', 'São Paulo', 'SP', 'Brasil', 'Av. Paulista, 1000', (SELECT id_empresa FROM empresa LIMIT 1));
 
--- Insere funcionários de teste
--- Senha para ambos é "password"
 INSERT INTO funcionario (nome_func, cpf_func, telefone_func, email_func, password, cargo, filial_id)
 VALUES ('Admin Chefe', '11122233344', '11999998888', 'admin@easymoto.com', '$2a$10$Y50UaMFOxteibQEYLrwuHeehHYfcoafCopUazP12.rqB41bsolF5.', 'ADMIN', (SELECT id_filial FROM filial LIMIT 1));
 
