2ff2e91299ed4a173fb957a2a7e7fce4090bc6cc
Lu1zEdu <le6269199@gmail.com>
2025-09-26 19:44:16 -0300

    Atualização para remover noticia e colocar auditoria

--- src/main/java/br/com/easymoto/repository/NoticiaRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.easymoto.repository;
-
-import br.com.easymoto.model.Noticia;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NoticiaRepository extends JpaRepository<Noticia, Long> {
-}
