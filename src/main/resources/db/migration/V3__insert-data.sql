23a395b4a88b6669f4168dc7ad48ce97fdd17795
Lu1zEdu <le6269199@gmail.com>
2025-09-26 22:40:15 -0300

    Atualização dos migrations

--- src/main/resources/db/migration/V3__insert-data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO cliente (nome_cliente, cpf_cliente, telefone_cliente, email_cliente) VALUES ('João Silva', '12345678901', '11987654321', 'joao.silva@example.com');
-INSERT INTO cliente (nome_cliente, cpf_cliente, telefone_cliente, email_cliente) VALUES ('Maria Souza', '12345678902', '11987654322', 'maria.souza@example.com');
-INSERT INTO empresa (nome_empresa, cnpj) VALUES ('Mottu', '12345678000199');
